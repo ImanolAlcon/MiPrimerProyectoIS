d13223634e5a06841acffd793fd8c097c34aa3c8
imano <imanol.alcon@gmail.com>
2021-01-28 14:57:34 +0100

    Nueva funcionalidad en el master

--- Ejemplo/src/MiPrimeraClase.java
+++ Ejemplo/src/MiPrimeraClase.java
@@ -3,8 +3,12 @@ public class MiPrimeraClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola como estas");
+		System.out.println("hola, vamos a liarla");
 
+	}
+	
+	public static void otroMetodo() {
+		System.out.print("Otro metodo");
 	}
 
 }
